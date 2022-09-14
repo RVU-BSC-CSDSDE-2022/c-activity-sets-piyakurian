b044b7f80474efd6f47bf87540f99198c06f3f57
Piya Sarah  Kurian <113001898+piyakurian@users.noreply.github.com>
2022-09-14 07:05:28 +0000

    learnt pass by ref

---
+++ set01/problem04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void input(int *a,int *b)
+{
+  printf("Enter two numbers");
+  scanf("%d%d",a,b);
+  }
+
+void add(int a,int b, int *c)
+{
+  *c = a+b;
+}
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  printf("%d + %d is %d\n",x,y,z);
+  return 0;
+}
