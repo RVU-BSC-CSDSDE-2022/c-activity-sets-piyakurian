9a9433ecbc93a4a481e9e460e724f71db9830317
Piya Sarah  Kurian <113001898+piyakurian@users.noreply.github.com>
2022-09-17 03:00:23 +0000

    practice

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,10 +1,11 @@
+
 #include <stdio.h>
-int main() 
+int main()
 {
   int a,b,c;
   printf("Enter two numbers\n");
- scanf("%d%d",&a,&b);
+  scanf("%d%d",&a,&b);
   c= a+b;
   printf("sum of %d and %d is %d\n",a,b,c);
   return 0;
-  }
+}
