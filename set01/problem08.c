755246c9992e58a256efd40b9acc058312bdc9fe
Piya Sarah  Kurian <113001898+piyakurian@users.noreply.github.com>
2022-11-11 14:04:19 +0000

    Typed without seeing

--- set01/problem08.c
+++ set01/problem08.c
@@ -1,28 +1,35 @@
-
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 
 int input_array_size()
-{  int n;
-  printf("Enter the numbers of variables you wish to find the sum of:");
-  scanf("%d", &n);
-    return n;
-}
-void input_array(int n, int a[n])
 {
-  for(int i=0;i<n;i++)
-  { printf("Enter the %d numbers you wish to find the sum of:",n);
+int n;
+printf("Enter the numbers of variables you wish to find the sum of\n");
+scanf("%D",&n);
+return n;
+}
+void input_array(int n,int a[n])
+{
+for(int i=0;i<n;i++)
+  {
+    printf("Enter the %d numbers you wish to find the sum of:\n",n);
     scanf("%d",&a[i]);
   }
 }
 int sum_n_array(int n, int a[n])
-{ int sum=0;
+{
+  int sum=0;
   for(int i=0;i<n;i++)
-    { sum = sum + a[i];}
-     return sum;
+    {
+      sum = sum + a[i];
+      return sum;
+    }
 }
 void output(int n, int a[n],int sum)
 {
   printf("The sum of the numbers of the array of size %d is %d",n,sum);
   for(int i=0;i<=n;i++)
- { printf("%d",a[i]);}
-  }
+    {
+      printf("%d",a[i]);
+    }
+}
